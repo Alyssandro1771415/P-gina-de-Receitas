483b199f43b46f324c4c488183b2979ab7ceac2b
Alyssandro Ramos <dyogo.alyssandro@gmail.com>
2023-01-10 00:07:04 -0300

    Página de Receitas
    Landing Page proposta pelo curso Full-Stack Programador BR.

---
+++ script.js
@@ -0,0 +1,13 @@
+function raiseOver(tag){
+
+    tag.style.height = "2cm";
+    tag.style.width = "6cm";
+
+}
+
+function smallOut(tag){
+
+    tag.style.height = "1cm";
+    tag.style.width = "5cm";
+
+}
